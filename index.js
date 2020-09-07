be76a38c8760785fe7e9622be5086176eae01b0e
Сергей Мамыкин <bymamykin@gmail.com>
2020-09-08 00:04:09 +0300

    maaa

---
+++ index.js
@@ -0,0 +1,24 @@
+$(document).ready(function (){
+    let open_modal = $('.menu_btn');
+    let close = $('.nav_mobile');
+    let menu = $('.nav_menu');
+    let checker = true;
+    open_modal.click(function (event){
+        event.preventDefault();
+        if(checker == true){
+        menu.css('display', 'block').animate({opacity:1,right: '5%'},500);
+        checker = false;
+        }else if(checker == false){
+            menu.animate({opacity: 0, right: '0%'}, 500);
+            setTimeout(()=>menu.css('display', 'none'),500);
+            checker = true;
+        }
+    })
+    close.click(function (event){
+        event.preventDefault();
+        menu.animate({opacity: 0, right: '0%'}, 500);
+       setTimeout(()=>menu.css('display', 'none'),500);
+       checker = true;
+    })
+})
+
